c81410e4e6515080d5dd0b61d19c539bcf386ecf
ObsidianMango <steve.ragucci@gmail.com>
2025-04-21 17:45:51 -0400

    Add files via upload

---
+++ apanic/sw.js
@@ -0,0 +1,6 @@
+// sw.js
+self.addEventListener('install', e => e.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e =>
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
+);
